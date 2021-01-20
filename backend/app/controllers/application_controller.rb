daeffce6a436b015c5854b5db518176c5e84debd
Joseph-Burks <joe_burks_3@yahoo.com>
2021-01-20 11:07:17 -0600

    Initial with some User

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::API
+    before_action :logged_in?
+
+    def current_user
+        begin
+            method, token = request.headers[:Authorization].split(' ')
+            if method === 'Bearer'
+                payload, header = JWT.decode(token, 'Hide this secret!')
+                return User.find(payload['user_id'])
+        rescue
+            raise Exception.new('You must be logged in to make this request.')
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+end
---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  skip_before_action :logged_in?, only: [:create, :log_in]
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user && @user.valid
+      @user.save
+      render json: {user: @user, token: JWT.encode({user_id: @user.id}, 'Hide this secret!')}, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def log_in
+    @user = User.find_by(username: user_params[:username])
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password)
+    end
+
+end
